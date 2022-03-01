209d1064f2cfe5d7d4be8e44214fdb25406e0d20
Wambuingare <wambuinancyngare@gmail.com>
2022-03-01 21:49:33 +0300

    W1

---
+++ src/main.kt
@@ -0,0 +1,28 @@
+fun main(){
+greetings()
+    modulus()
+    sum()
+    funfact()
+}
+fun greetings(){
+    var name = "Nancy"
+    println("Hello $name")
+}
+fun modulus(){
+    var a = 12
+    var b = 5
+    var modulus = a%b
+    println(modulus)
+}
+fun sum (){
+    var a = 2
+    var b = 7
+    var c = 3
+    var d = 8
+    var sum = a+b+c+d
+    println(sum)
+}
+fun funfact (){
+    var funfact = "I like coding"
+    println(funfact)
+}
